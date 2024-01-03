d559cf689a3ba6c5cfd44386fc6a7e7d2bb8b96c
Yibing <48507231+YibingZeng@users.noreply.github.com>
2024-01-03 15:10:45 -0500

    Create 02_bwa_index.sh

---
+++ Mapping/02_bwa_index.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+#SBATCH --job-name=build_index                     
+#SBATCH --partition=batch		                            
+#SBATCH --ntasks=1			                            
+#SBATCH --cpus-per-task=4		                       
+#SBATCH --mem=400gb			                               
+#SBATCH --time=168:00:00  		                          
+#SBATCH --output=trim_adaptor.out			  
+#SBATCH --error=trim_adaptor.err
+
+############################################
+##Bulid the index ####
+############################################
+
+ml BWA/0.7.17-GCCcore-11.3.0
+cd /scratch/yz77862/MaizeGenome
+ABS_assembly=/scratch/yz77862/MaizeGenome/AbsGenomePBHIFI_version_1.fa
+A188=/scratch/yz77862/MaizeGenome/Zm-A188-REFERENCE-KSU-1.0.fa
+B73=/scratch/yz77862/MaizeGenome/Zm-B73-REFERENCE-NAM-5.0.fa
+Mo17=/scratch/yz77862/MaizeGenome/Zm-Mo17-REFERENCE-CAU-2.0.fa
+W22=/scratch/yz77862/MaizeGenome/Zm-W22-REFERENCE-NRGENE-2.0.fa
+
+#####Change the naming of chr in genome files
+for i in 1 2 3;do
+sed 's/chr'${i}'/chr'${i}'_B73//g' ${chr.txt}
+done
+
+###Concat two different genome together
+cat ${ABS} ${A188} > ABS_A188.fa
+cat ${ABS} ${B73} > ABS_B73.fa
+cat ${ABS} ${Mo17} > ABS_Mo17.fa
+cat ${ABS} ${W22} > ABS_W22.fa
+
+cat ${A188} ${B73} > A188_B73.fa
+cat ${A188} ${Mo17} > A188_Mo17.fa
+cat ${A188} ${W22} > A188_W22.fa
+
+cat ${B73} ${Mo17} > B73_Mo17.fa
+cat ${B73} ${W22} > B73_W22.fa
+
+cat ${Mo17} ${W22} > Mo17_W22.fa
+
+for i in *fa;do bwa index ${i};done
+
+
+########################################################################################
+####Mapping####
+########################################################################################
+ml BWA/0.7.17-GCCcore-11.3.0
+ml BEDTools/2.29.2-GCC-8.3.0
+ml SAMtools/1.16.1-GCC-11.3.0
+ml IGV/2.16.1-Java-11
+
+cd /scratch/yz77862/illumina_neo4Ls
+for i in J721 KD4277 KD4289-6 KD4300-7 KD4303-1 KD4303-2 KD4304-1 L6_R1_001 ;do
+fastq1=/scratch/yz77862/illumina_neo4Ls/${i}_R1_001.fastq.gz.trimmed.fastq.gz
+fastq2=/scratch/yz77862/illumina_neo4Ls/${i}_R2_001.fastq.gz.trimmed.fastq.gz
+bwa mem ${ABS_assembly} ${fastq1} ${fastq2} -M -t 24  > ${i}_ABS.sam
+bwa mem ${A188} ${fastq1} ${fastq2} -M -t 24  > ${i}_A188.sam
+bwa mem ${B73} ${fastq1} ${fastq2} -M -t 24  > ${i}_B73.sam
+bwa mem ${Mo17} ${fastq1} ${fastq2} -M -t 24  > ${i}_Mo17.sam
+
+samtools view -b -F 4 -S ${i}.sam -o ${i}.bam 
+samtools sort -o ${i}.sorted.bam ${i}.bam 
+samtools view -q 20 -o ${i}_q20.bam ${i}.sorted.bam
+samtools index ${i}.sorted.bam
+samtools index ${i}.sorted_q20.bam
+touch flagstat_result.txt
+echo '${Input}.sorted.bam' >> flagstat_result.txt
+samtools flagstat ${i}.sorted.bam >> flagstat_result.txt
+echo '${Input}.sorted_q20.bam' >> flagstat_result.txt
+samtools flagstat ${i}.sorted.bam >> flagstat_result.txt
+bedtools bamtobed -cigar -i ${i}.sorted.bam > ${i}.sorted.bed
+bedtools bamtobed -cigar -i ${i}.sorted_q20.bam > ${i}.sorted_q20.bed
+igvtools count -w 100000 ${i}.sorted.bam ${i}.20Kb.tdf ragtag.scaffold.fasta
+igvtools count -w 100000 ${i}.sorted_q20.bam ${i}.q20.20Kb.tdf ragtag.scaffold.fasta
+
+bedtools bamtobed -cigar -i ${i}.sorted.bam > ${i}.sorted.bed
+bedtools bamtobed -cigar -i ${i}.sorted_q20.bam > ${i}.sorted_q20.bed
+igvtools count -w 100000 ${i}.sorted.bam ${i}.20Kb.tdf ragtag.scaffold.fasta
+igvtools count -w 100000 ${i}.sorted_q20.bam ${i}.q20.20Kb.tdf ${genome}
+
+done
+
+het1=/scratch/yz77862/ABS_PacBio_version1/LexA_CENH3_abs4_S23_R1_001.fastq.gz.trimmed.fastq.gz
+het2=/scratch/yz77862/ABS_PacBio_version1/LexA_CENH3_abs4_S23_R2_001.fastq.gz.trimmed.fastq.gz
+W22=/scratch/yz77862/ABS_PacBio_version1/W22-6.fastq.gz.trimmed.fastq.gz
+
+
+
+done
