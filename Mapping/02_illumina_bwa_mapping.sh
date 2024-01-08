9933cd29f21c4b90eca547061177218e58c6a2b8
Yibing <48507231+YibingZeng@users.noreply.github.com>
2024-01-08 09:19:48 -0500

    Update 02_illumina_bwa_mapping.sh

--- Mapping/02_illumina_bwa_mapping.sh
+++ Mapping/02_illumina_bwa_mapping.sh
@@ -2,8 +2,8 @@ mkdir -p /scratch/yz77862/illumina_neo4Ls/shell
 for j in ABS_A188 ABS_B73 ABS_Mo17 ABS_W22 A188_B73 A188_Mo17 A188_W22 B73_Mo17 B73_W22 Mo17_W22 AbsGenomePBHIFI_version_1_addname Zm-A188-REFERENCE-KSU-1.0_addname Zm-B73-REFERENCE-NAM-5.0_addname Zm-Mo17-REFERENCE-CAU-2.0_addname Zm-W22-REFERENCE-NRGENE-2.0_addname;do 
   for i in J721 KD4277 KD4289-6 KD4300-7 KD4303-1 KD4303-2 KD4304-1 L6 LexA_CENH3_abs4_S23;do
 genome=/scratch/yz77862/MaizeGenome/${j}.fa
-fastq1=/scratch/yz77862/illumina_neo4Ls\${i}_R1_001.fastq.gz.trimmed.fastq.gz
-fastq2=/scratch/yz77862/illumina_neo4Ls\${i}_R2_001.fastq.gz.trimmed.fastq.gz 
+fastq1=/scratch/yz77862/illumina_neo4Ls/${i}_R1_001.fastq.gz.trimmed.fastq.gz
+fastq2=/scratch/yz77862/illumina_neo4Ls/${i}_R2_001.fastq.gz.trimmed.fastq.gz 
 
 out=/scratch/yz77862/illumina_neo4Ls/shell/${i}_${j}_map.sh
 echo '#!/bin/bash' >> ${out}
@@ -22,14 +22,11 @@ echo "ml SAMtools/1.16.1-GCC-11.3.0" >> ${out}
 echo "ml IGV/2.16.1-Java-11" >> ${out}  
 echo "mkdir -p /scratch/yz77862/illumina_neo4Ls/${i}_${j}" >> ${out}  
 echo "cd /scratch/yz77862/illumina_neo4Ls/${i}_${j}" >> ${out}  
-#echo "for i in J721 KD4277 KD4289-6 KD4300-7 KD4303-1 KD4303-2 KD4304-1 L6 LexA_CENH3_abs4_S23;do" >> ${out}  
-#echo "fastq1=/scratch/yz77862/illumina_neo4Ls/\${i}_R1_001.fastq.gz.trimmed.fastq.gz" >> ${out}  
-#echo "fastq2=/scratch/yz77862/illumina_neo4Ls/\${i}_R2_001.fastq.gz.trimmed.fastq.gz" >> ${out}  
 echo "bwa mem ${genome} ${fastq1} ${fastq2} -M -t 24  > ${j}_${i}_ABS.sam" >> ${out}  
 echo " " >> ${out}  
-echo "samtools view -b -F 4 -S ${j}_\${i}_ABS.sam -o ${j}_${i}_ABS.bam" >> ${out}  
-echo "samtools sort -o ${j}_\${i}_ABS.sorted.bam  ${j}_${i}_ABS.bam" >> ${out}    
-echo "samtools view -q 20 -o ${j}_$\{i}_ABS.sorted_q20.bam ${j}_${i}_ABS.sorted.bam" >> ${out}  
+echo "samtools view -b -F 4 -S ${j}_${i}_ABS.sam -o ${j}_${i}_ABS.bam" >> ${out}  
+echo "samtools sort -o ${j}_${i}_ABS.sorted.bam  ${j}_${i}_ABS.bam" >> ${out}    
+echo "samtools view -q 20 -o ${j}_${i}_ABS.sorted_q20.bam ${j}_${i}_ABS.sorted.bam" >> ${out}  
 echo "samtools index ${j}_${i}_ABS.sorted.bam" >> ${out}  
 echo "samtools index ${j}_${i}_ABS.sorted_q20.bam" >> ${out}  
 echo "touch flagstat_result.txt" >> ${out}  
@@ -43,7 +40,7 @@ echo "igvtools count -w 100000 ${j}_${i}_ABS.sorted.bam ${j}_${i}_ABS.sorted_q20
 echo "igvtools count -w 100000 ${j}_${i}_ABS.sorted_q20.bam ${j}_${i}_ABS.sorted_q20.20Kb.tdf ${genome}" >> ${out}  
 echo " " >> ${out}  
 echo "W22=/scratch/yz77862/illumina_neo4Ls/W22-6.fastq.gz.trimmed.fastq.gz" >> ${out}  
-echo "bwa mem ${genome} ${W22} -M -t 24  > ${j}_W22-6_ABS.sam" >> ${out}  
+echo "bwa mem ${genome} \${W22} -M -t 24  > ${j}_W22-6_ABS.sam" >> ${out}  
 echo " " >> ${out}  
 echo "samtools view -b -F 4 -S ${j}_W22-6_ABS.sam -o ${j}_W22-6_ABS.bam" >> ${out}   
 echo "samtools sort -o ${j}_W22-6_ABS.sorted.bam  ${j}_W22-6_ABS.bam" >> ${out}    
@@ -61,3 +58,4 @@ echo "igvtools count -w 100000 ${j}_W22-6_ABS.sorted.bam ${j}_W22-6_ABS.sorted_q
 echo "igvtools count -w 100000 ${j}_W22-6_ABS.sorted_q20.bam ${j}_W22-6_ABS.sorted_q20.20Kb.tdf ${genome}" >> ${out}  
 done
   done
+#for i in *sh;do sbatch $i;done
