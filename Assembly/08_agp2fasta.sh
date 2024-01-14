985207272ad7fdf4bfb5c37740ea39f696659212
Yibing <48507231+YibingZeng@users.noreply.github.com>
2024-01-13 20:26:06 -0500

    Update 08_agp2fasta.sh

--- Assembly/08_agp2fasta.sh
+++ Assembly/08_agp2fasta.sh
@@ -1,3 +1,4 @@
 ##The final_scaffold.agp is generated based on 06 R script 
 ml RagTag/2.1.0
-ragtag_agp2fa.py final_scaffold.agp Zmays_Abs4-2cells_hifiasm_l0.bp.p_ctg.fa > ABS_version_1.fa
+cd /scratch/yz77862/ABS_PacBio_version1
+ragtag_agp2fa.py final_scaffold.agp Zmays_Abs4-2cells_hifiasm_l0.bp.p_ctg.fa > AbsGenomePBHIFI_version_1.fa
