3d8cc080ae1be633e756b06ef1cce0cc1261f45c
Yibing <48507231+YibingZeng@users.noreply.github.com>
2024-01-16 18:04:04 -0500

    Update 02_TrimAdaptor.sh

--- Mapping/02_TrimAdaptor.sh
+++ Mapping/02_TrimAdaptor.sh
@@ -1,2 +1,16 @@
-echo "##Trim the adaptors" >> ${out}   
-echo "#trim_galore --fastqc --gzip --paired ${i}_1.fastq ${i}_2.fastq -o . -a CTGTCTCTTATACACATCT" >> ${out}   
+#!/bin/bash
+#SBATCH --job-name=TrimAdaptor              
+#SBATCH --partition=batch		                            
+#SBATCH --ntasks=1			                            
+#SBATCH --cpus-per-task=4 		                       
+#SBATCH --mem=400gb  			                               
+#SBATCH --time=168:00:00   		                          
+#SBATCH --output=TrimAdaptor.out			  
+#SBATCH --error=TrimAdaptor.err
+
+ml Trim_Galore/0.6.7-GCCcore-11.2.0 
+cd /scratch/yz77862/CUTnTag_neo4Ls/data/
+list=/scratch/yz77862/CUTnTag_neo4Ls/data/list
+while read GENOME;do
+trim_galore --fastqc --gzip --paired ${GENOME}_R1_001.fastq.gz ${GENOME}_R2_001.fastq.gz -o . -a CTGTCTCTTATACACATCT  
+done < <(cut -f1 ${list} | grep -v 'skip' | sort -u)
