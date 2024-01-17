0fc83b44be74899fe91d9381d581fe2933a29aae
Yibing <48507231+YibingZeng@users.noreply.github.com>
2024-01-17 11:40:24 -0500

    Create 05_makewindows.sh

---
+++ Mapping/05_makewindows.sh
@@ -0,0 +1,7 @@
+genome_fai=/scratch/yz77862/ABS_PacBio_version1/AbsGenomePBHIFI_version_1.fa.fai
+#Only extract the chr size
+head ${genome_fai} > genome_size
+bedtools makewindows -g genome_size -w 10000 > AbsGenomePBHIFI_version_1_10k_win.bed
+bedtools makewindows -g genome_size -w 25000 > AbsGenomePBHIFI_version_1_25k_win.bed
+bedtools makewindows -g genome_size -w 50000 > AbsGenomePBHIFI_version_1_50k_win.bed
+bedtools makewindows -g genome_size -w 100000 > AbsGenomePBHIFI_version_1_100k_win.bed
