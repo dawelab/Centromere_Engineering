223340ba6ebf66276ee9990d08d980eeb3f17ce5
Yibing <48507231+YibingZeng@users.noreply.github.com>
2024-04-04 09:38:11 -0400

    Update 08_Map_Tx77_L1_del.sh

--- Mapping/08_Map_Tx77_L1_del.sh
+++ Mapping/08_Map_Tx77_L1_del.sh
@@ -1,1 +1,16 @@
 
+echo '#!/bin/bash' >> ${out}
+#SBATCH --job-name=${i}_normalization                
+#SBATCH --partition=batch  		                            
+#SBATCH --ntasks=1  			                            
+#SBATCH --cpus-per-task=4  		                       
+#SBATCH --mem=400gb  			                               
+#SBATCH --time=20:00:00    		                          
+#SBATCH --output=${i}_normalization.out  			  
+#SBATCH --error=${i}_normalization.err  
+   
+cd /scratch/yz77862/CUTnTag_neo4Ls/output/genomecov  
+BAMQ20=/scratch/yz77862/CUTnTag_neo4Ls/output/BAMQ20/${i}.sorted_q20.bam  
+BAM=/scratch/yz77862/CUTnTag_neo4Ls/output/BAM/${i}.sorted.bam  
+ 
+index=/scratch/yz77862/MaizeGenome/
