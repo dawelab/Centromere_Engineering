4dfc796a58b8ec5ce460790c33cde96dc07de270
Yibing <48507231+YibingZeng@users.noreply.github.com>
2024-04-25 12:04:39 -0400

    Update 09_Map_YZlist.sh

--- Mapping/09_Map_YZlist.sh
+++ Mapping/09_Map_YZlist.sh
@@ -31,28 +31,59 @@ echo "win_10k=/scratch/yz77862/ABS_PacBio_version1/AbsGenomePBHIFI_version_1_10k
 echo "win_25k=/scratch/yz77862/ABS_PacBio_version1/AbsGenomePBHIFI_version_1_25k_win.bed" >> ${out} 
 echo "win_50k=/scratch/yz77862/ABS_PacBio_version1/AbsGenomePBHIFI_version_1_50k_win.bed" >> ${out} 
 echo "win_100k=/scratch/yz77862/ABS_PacBio_version1/AbsGenomePBHIFI_version_1_100k_win.bed" >> ${out} 
-echo "  " >> ${out}
+echo " #The output file lists " >> ${out}
 echo "SAM=/scratch/yz77862/illumina_neo4Ls/output/SAM" >> ${out}
 echo "BAM=/scratch/yz77862/illumina_neo4Ls/output/BAM" >> ${out}
 echo "BAMQ20=/scratch/yz77862/illumina_neo4Ls/output/BAMQ20 " >> ${out}
 echo "BED=/scratch/yz77862/illumina_neo4Ls/output/BED" >> ${out}
 echo "BEDQ20=/scratch/yz77862/illumina_neo4Ls/output/BEDQ20" >> ${out}
 echo "TDF=/scratch/yz77862/illumina_neo4Ls/output/TDF" >> ${out}
+echo "genomecov=/scratch/yz77862/illumina_neo4Ls/output/genomecov" >> ${out}
 echo "  " >> ${out}
-echo "bwa mem ${genome} ${fastq1} ${fastq2} -M -t 24  > ${SAM}/${i}_ABS.sam" >> ${out}  
-echo "samtools view -b -F 4 -S ${SAM}/${i}_ABS.sam -o ${BAM}/${i}_ABS.bam" >> ${out}  
-echo "samtools sort -o ${BAM}/${i}_ABS.sorted.bam ${BAM}/${i}_ABS.bam" >> ${out}    
-echo "samtools view -q 20 -o ${BAMQ20}/${i}_ABS.sorted_q20.bam ${BAM}/${i}_ABS.sorted.bam" >> ${out}  
-echo "samtools index ${BAM}/${i}_ABS.bam" >> ${out}  
-echo "samtools index ${BAMQ20}/${i}_ABS.sorted_q20.bam" >> ${out}  
+echo "bwa mem \${genome} \${fastq1} \${fastq2} -M -t 24  > \${SAM}/${i}_ABS.sam" >> ${out}  
+echo "samtools view -b -F 4 -S \${SAM}/${i}_ABS.sam -o \${BAM}/${i}_ABS.bam" >> ${out}  
+echo "samtools sort -o \${BAM}/${i}_ABS.sorted.bam \${BAM}/${i}_ABS.bam" >> ${out}    
+echo "samtools view -q 20 -o \${BAMQ20}/${i}_ABS.sorted_q20.bam \${BAM}/${i}_ABS.sorted.bam" >> ${out}  
+echo "samtools index \${BAM}/${i}_ABS.bam" >> ${out}  
+echo "samtools index \${BAMQ20}/${i}_ABS.sorted_q20.bam" >> ${out}  
 echo "touch flagstat_result.txt" >> ${out}  
-echo "echo '${i}_ABS.sorted.bam' >> flagstat_result.txt" >> ${out}  
-echo "samtools flagstat ${BAM}/${i}_ABS.sorted.bam >> flagstat_result.txt" >> ${out}  
-echo "echo '${BAMQ20}/${i}_ABS.sorted_q20.bam' >> flagstat_result.txt" >> ${out}  
-echo "samtools flagstat ${i}_ABS.sorted_q20.bam >> flagstat_result.txt" >> ${out}  
-echo "bedtools bamtobed -cigar -i ${i}_ABS.sorted.bam > _${i}_ABS.sorted.bed" >> ${out}  
+echo "echo '\${BAM}/${i}_ABS.sorted.bam' >> flagstat_result.txt" >> ${out}  
+echo "samtools flagstat \${BAM}/${i}_ABS.sorted.bam >> flagstat_result.txt" >> ${out}  
+echo "echo '${i}_ABS.sorted_q20.bam' >> flagstat_result.txt" >> ${out}  
+echo "samtools flagstat \${BAMQ20}/${i}_ABS.sorted_q20.bam >> flagstat_result.txt" >> ${out}  
+echo "bedtools bamtobed -cigar -i \${BAM}/${i}_ABS.sorted.bam > \${BED}/${i}_ABS.sorted.bed" >> ${out}  
 echo "bedtools bamtobed -cigar -i ${i}_ABS.sorted_q20.bam > _${i}_ABS.sorted_q20.bed" >> ${out}  
-echo "igvtools count -w 10000 ${BAM}/${i}_ABS.sorted.bam ${TDF}/${i}_ABS.sorted_q20.10Kb.tdf ${genome}" >> ${out}  
-echo "igvtools count -w 10000 ${BAMQ20}/${i}_ABS.sorted_q20.bam ${TDF}/${i}_ABS.sorted_q20.10Kb.tdf ${genome}" >> ${out} 
+echo "igvtools count -w 10000 \${BAM}/${i}_ABS.sorted.bam \${TDF}/${i}_ABS.sorted_q20.10Kb.tdf ${genome}" >> ${out}  
+echo "igvtools count -w 10000 \${BAMQ20}/${i}_ABS.sorted_q20.bam \${TDF}/${i}_ABS.sorted_q20.10Kb.tdf ${genome}" >> ${out} 
+echo "  " >> ${out}
+echo "bedtools genomecov -ibam \${BAM}/${i}_ABS.sorted.bam -bg > ${genomecov}/${i}_genomecov.bed" >> ${out}
+echo "bedtools genomecov -ibam  \${BAMQ20}/${i}_ABS.sorted_q20.bam -bg > ${genomecov}/${i}_q20_genomecov.bed" >> ${out}
+echo "bedtools intersect -wa -wb -a \${win_10k} -b \${genomecov}/${i}_genomecov.bed | bedtools groupby -c 7 -o sum > \${genomecov}/${i}_win_10k_genomecov.bed1" >> ${out}
+echo "bedtools intersect -wa -wb -a \${win_10k} -b \${genomecov}/${i}_q20_genomecov.bed | bedtools groupby -c 7 -o sum > \${genomecov}/${i}_win_10k_q20_genomecov.bed1" >> ${out}
+echo "bedtools intersect -wa -wb -a \${win_25k} -b \${genomecov}/${i}_genomecov.bed | bedtools groupby -c 7 -o sum > \${genomecov}/${i}_win_25k_genomecov.bed1" >> ${out}
+echo "bedtools intersect -wa -wb -a \${win_25k} -b \${genomecov}/${i}_q20_genomecov.bed | bedtools groupby -c 7 -o sum > \${genomecov}/${i}_win_q20_25k_genomecov.bed1" >> ${out}
+echo "bedtools intersect -wa -wb -a \${win_50k} -b \${genomecov}/${i}_genomecov.bed | bedtools groupby -c 7 -o sum > \${genomecov}/${i}_win_50k_genomecov.bed1" >> ${out}
+echo "bedtools intersect -wa -wb -a \${win_50k} -b \${genomecov}/${i}_q20_genomecov.bed | bedtools groupby -c 7 -o sum > \${genomecov}/${i}_win_50k_q20_genomecov.bed1" >> ${out}
+echo "bedtools intersect -wa -wb -a \${win_100k} -b \${genomecov}/${i}_genomecov.bed | bedtools groupby -c 7 -o sum > \${genomecov}/${i}_win_100k_genomecov.bed1" >> ${out}
+echo "bedtools intersect -wa -wb -a \${win_100k} -b \${genomecov}/${i}_q20_genomecov.bed | bedtools groupby -c 7 -o sum > \${genomecov}/${i}_win_100k_q20_genomecov.bed1" >> ${out}
+echo " " >> ${out} 
+echo "bedtools intersect -wa -wb -a \${win_10k} -b \${genomecov}/${i}_win_10k_genomecov.bed1 -v | awk '{print \$1,\$2,\$3,0}' OFS="\t"> \${genomecov}/${i}_win_10k_genomecov.bed2" >> ${out} 
+echo "bedtools intersect -wa -wb -a \${win_10k} -b \${genomecov}/${i}_win_10k_q20_genomecov.bed1 -v | awk '{print \$1,\$2,\$3,0}' OFS="\t"> \${genomecov}/${i}_win_10k_q20_genomecov.bed2" >> ${out} 
+echo "bedtools intersect -wa -wb -a \${win_25k} -b \${genomecov}/${i}_win_25k_genomecov.bed1 -v | awk '{print \$1,\$2,\$3,0}' OFS="\t" > \${genomecov}/${i}_win_25k_genomecov.bed2" >> ${out} 
+echo "bedtools intersect -wa -wb -a \${win_25k} -b \${genomecov}/${i}_win_q20_25k_genomecov.bed1 -v | awk '{print \$1,\$2,\$3,0}' OFS="\t" > \${genomecov}/${i}_win_q20_25k_genomecov.bed2" >> ${out} 
+echo "bedtools intersect -wa -wb -a \${win_50k} -b \${genomecov}/${i}_win_50k_genomecov.bed1 -v | awk '{print \$1,\$2,\$3,0}' OFS="\t" > \${genomecov}/${i}_win_50k_genomecov.bed2 ">> ${out} 
+echo "bedtools intersect -wa -wb -a \${win_50k} -b \${genomecov}/${i}_win_50k_q20_genomecov.bed1 -v | awk '{print \$1,\$2,\$3,0}' OFS="\t" > \${genomecov}/${i}_win_50k_q20_genomecov.bed2" >> ${out} 
+echo "bedtools intersect -wa -wb -a \${win_100k} -b \${genomecov}/${i}_win_100k_genomecov.bed1 -v | awk '{print \$1,\$2,\$3,0}' OFS="\t" > \${genomecov}/${i}_win_100k_genomecov.bed2" >> ${out} 
+echo "bedtools intersect -wa -wb -a \${win_100k} -b \${genomecov}/${i}_win_100k_q20_genomecov.bed1 -v | awk '{print \$1,\$2,\$3,0}' OFS="\t" > \${genomecov}/${i}_win_100k_q20_genomecov.bed2" >> ${out} 
+echo " " >> ${out} 
+echo "cat \${genomecov}/${i}_win_10k_genomecov.bed1 \${genomecov}/${i}_win_10k_genomecov.bed2 | sort -b -k1,1 -k2,2n -k3,3n  > \${genomecov}/${i}_win_10k_genomecov.bed" >> ${out} 
+echo "cat \${genomecov}/${i}_win_10k_q20_genomecov.bed1 \${genomecov}/${i}_win_10k_q20_genomecov.bed2 | sort -b -k1,1 -k2,2n -k3,3n > \${genomecov}/${i}_win_10k_q20_genomecov.bed" >> ${out} 
+echo "cat \${genomecov}/${i}_win_25k_genomecov.bed1 \${genomecov}/${i}_win_25k_genomecov.bed2 | sort -b -k1,1 -k2,2n -k3,3n > \${genomecov}/${i}_win_25k_genomecov.bed " >> ${out} 
+echo "cat \${genomecov}/${i}_win_q20_25k_genomecov.bed1 \${genomecov}/${i}_win_q20_25k_genomecov.bed2 | sort -b -k1,1 -k2,2n -k3,3n  > \${genomecov}/${i}_win_q20_25k_genomecov.bed" >> ${out} 
+echo "cat \${genomecov}/${i}_win_50k_genomecov.bed1 \${genomecov}/${i}_win_50k_genomecov.bed2 | sort -b -k1,1 -k2,2n -k3,3n > \${genomecov}/${i}_win_50k_genomecov.bed" >> ${out} 
+echo "cat \${genomecov}/${i}_win_50k_q20_genomecov.bed1 \${genomecov}/${i}_win_50k_q20_genomecov.bed2 | sort -b -k1,1 -k2,2n -k3,3n  > \${genomecov}/${i}_win_50k_q20_genomecov.bed" >> ${out} 
+echo "cat \${genomecov}/${i}_win_100k_genomecov.bed1 \${genomecov}/${i}_win_100k_genomecov.bed2  | sort -b -k1,1 -k2,2n -k3,3n  > \${genomecov}/${i}_win_100k_genomecov.bed " >> ${out} 
+echo "cat \${genomecov}/${i}_win_100k_q20_genomecov.bed1 \${genomecov}/${i}_win_100k_q20_genomecov.bed2 | sort -b -k1,1 -k2,2n -k3,3n > \${genomecov}/${i}_win_100k_q20_genomecov.bed" >> ${out} 
+done < <(cut -f1 ${list} | grep -v 'skip' | sort -u)
 
 echo " " >> ${out}  
