01f86fcdd98d09e4ef11d8d3e4eb3d08e413bc4f
Yibing <48507231+YibingZeng@users.noreply.github.com>
2024-05-08 21:50:51 -0400

    Update 05_makewindows.sh

--- Mapping/05_makewindows.sh
+++ Mapping/05_makewindows.sh
@@ -2,7 +2,10 @@ ml BEDTools
 genome_fai=/scratch/yz77862/ABS_PacBio_version1/AbsGenomePBHIFI_version_1.fa.fai
 #Only extract the chr size
 head ${genome_fai} > genome_size
-bedtools makewindows -g genome_size -w 10000 > AbsGenomePBHIFI_version_1_10k_win.bed
-bedtools makewindows -g genome_size -w 25000 > AbsGenomePBHIFI_version_1_25k_win.bed
-bedtools makewindows -g genome_size -w 50000 > AbsGenomePBHIFI_version_1_50k_win.bed
 bedtools makewindows -g genome_size -w 100000 > AbsGenomePBHIFI_version_1_100k_win.bed
+
+Mo17_fai=/scratch/yz77862/MaizeGenome/Zm-Mo17-REFERENCE-CAU-2.0_addname.fa.fai
+head ${Mo17_fai} > Mo17_genome_size
+bedtools makewindows -g Mo17_genome_size -w 100000 > Mo17_100k_win.bed
+
+W22_fai=/scratch/yz77862/MaizeGenome/
