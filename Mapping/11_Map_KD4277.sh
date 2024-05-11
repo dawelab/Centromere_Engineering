7a9d2e722d8f6481faf5db965db7a48db3570272
Yibing <48507231+YibingZeng@users.noreply.github.com>
2024-05-11 17:52:56 -0400

    Update 11_Map_KD4277.sh

--- Mapping/11_Map_KD4277.sh
+++ Mapping/11_Map_KD4277.sh
@@ -1,1 +1,8 @@
 
+
+
+bedtools genomecov -ibam  \${BAMQ20}/KD4277_ABS.sorted_q20.bam -bg | awk '{print \$0,(\$3-\$2)*\$4}' OFS="\t" > \${genomecov}/KD4277_q20_genomecov.bed
+bedtools intersect -wa -wb -a \${win_100k} -b \${genomecov}/KD4277_q20_genomecov.bed | bedtools groupby -c 8 -o sum > \${genomecov}/KD4277_win_100k_q20_genomecov.bed1
+bedtools intersect -wa -wb -a \${win_100k} -b \${genomecov}/KD4277_q20_genomecov.bed -v | awk '{print \$1,\$2,\$3,0}' OFS="\t" > \${genomecov}/KD4277_win_100k_q20_genomecov.bed2 
+cat \${genomecov}/KD4277_win_100k_q20_genomecov.bed1 \${genomecov}/KD4277_win_100k_q20_genomecov.bed2 | sort -b -k1,1 -k2,2n -k3,3n > \${genomecov}/KD4277_win_100k_q20_genomecov.bed 
+rm \${genomecov}/KD4277_win_100k_q20_genomecov.bed1 \${genomecov}/KD4277_win_100k_q20_genomecov.bed2 
