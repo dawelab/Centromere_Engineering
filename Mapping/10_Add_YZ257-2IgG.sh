d788cd9063e954028e540772fcfcf0ee525c08cf
Yibing <48507231+YibingZeng@users.noreply.github.com>
2024-05-13 16:14:55 -0400

    Update 10_Add_YZ257-2IgG.sh

--- Mapping/10_Add_YZ257-2IgG.sh
+++ Mapping/10_Add_YZ257-2IgG.sh
@@ -17,8 +17,21 @@ win_100k=/scratch/yz77862/ABS_PacBio_version1/AbsGenomePBHIFI_version_1_100k_win
 YZ257_2=/scratch/yz77862/CUTnTag_neo4Ls/output/BAM/yz252-7-IgG.sorted.bam
 YZ257_2_q20=/scratch/yz77862/CUTnTag_neo4Ls/output/BAMQ20/yz252-7-IgG.sorted_q20.bam
 
-bedtools intersect 
-for i in *ABS.sorted_q20.bam;do 
-bedtools coverage -a ${win_100k} -b ${i} -sorted -f 0.5 > ${working_dir_ABS}/${i}.cov
-done
 
+bedtools genomecov -ibam  ${YZ257_2} -bg | awk '{print $0,($3-$2)*$4}' OFS="\t" > yz252-7-IgG.sorted.bg.bed
+bedtools genomecov -ibam  ${YZ257_2_q20} -bg | awk '{print $0,($3-$2)*$4}' OFS="\t" > yz252-7-IgG.sorted_q20.bg.bed
+
+bedtools intersect -wa -wb -a ${win_100k} -b yz252-7-IgG.sorted.bg.bed | bedtools groupby -c 8 -o sum > yz252-7-IgG.sorted.bg.bed1
+bedtools intersect -wa -wb -a ${win_100k} -b yz252-7-IgG.sorted.bg.bed -v | bedtools groupby -c 8 -o sum > yz252-7-IgG.sorted.bg.bed2
+
+cat yz252-7-IgG.sorted.bg.bed1 yz252-7-IgG.sorted.bg.bed2 | sort -b -k1,1 -k2,2n -k3,3n > yz252-7-IgG.sorted.bg_win_100k.bed
+rm yz252-7-IgG.sorted.bg.bed1 yz252-7-IgG.sorted.bg.bed2
+
+bedtools intersect -wa -wb -a ${win_100k} -b yz252-7-IgG.sorted_q20.bg.bed | bedtools groupby -c 8 -o sum > yz252-7-IgG.sorted_q20.bg.bed1
+bedtools intersect -wa -wb -a ${win_100k} -b yz252-7-IgG.sorted_q20.bg.bed -v | bedtools groupby -c 8 -o sum > yz252-7-IgG.sorted_q20.bg.bed2
+
+cat yz252-7-IgG.sorted_q20.bg.bed1 yz252-7-IgG.sorted_q20.bg.bed2 | sort -b -k1,1 -k2,2n -k3,3n > yz252-7-IgG.sorted_q20.bg_win_100k.bed
+rm yz252-7-IgG.sorted_q20.bg.bed1 yz252-7-IgG.sorted_q20.bg.bed2 
+
+bedtools coverage -a ${win_100k} -b ${YZ257_2} -sorted -f 0.5 > yz252-7-IgG.sorted_win_100k_raw_count.bed
+bedtools coverage -a ${win_100k} -b ${YZ257_2_q20} -sorted -f 0.5 > yz252-7-IgG.sorted_win_100k_raw_count_q20.bed
