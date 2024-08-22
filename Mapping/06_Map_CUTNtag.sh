c7b17dd298d192fa8b2036db173c44d3f72be662
Yibing <48507231+YibingZeng@users.noreply.github.com>
2024-08-22 17:26:45 -0400

    Update 06_Map_CUTNtag.sh

--- Mapping/06_Map_CUTNtag.sh
+++ Mapping/06_Map_CUTNtag.sh
@@ -4,13 +4,13 @@
 mkdir -p /scratch/yz77862/CUTnTag/
 cd /scratch/yz77862/CUTnTag/
 for i in ABS Mo17 W22;do #For different mapping 
-  for j in SAM BAM BAMQ20 BED BEDQ20 genomecov ratio TDF;do
+  for j in SAM BAM BAMQ20 BED BEDQ20 genomecov ratio TDF shell;do
   mkdir -p ${i}/${j}
   done
 done
 list=/scratch/yz77862/CUTnTag/data/list
 while read i;do
-out=${i}_mapping.sh
+out=/scratch/yz77862/CUTnTag/shell/${i}_mapping.sh
 echo '#!/bin/bash' >> ${out}
 echo "#SBATCH --job-name=${i}_mapping" >> ${out}                 
 echo "#SBATCH --partition=batch" >> ${out}   		                            
@@ -27,7 +27,7 @@ echo "ml SAMtools/1.16.1-GCC-11.3.0" >> ${out}
 echo "ml IGV/2.16.1-Java-11" >> ${out} 
 echo " " >> ${out} 
 #The main working directory
-cd /scratch/yz77862/CUTnTag
+echo "cd /scratch/yz77862/CUTnTag " >> ${out} 
 #######################################################
 #####Set up the value for different mapping genome#####
 #######################################################
@@ -48,13 +48,13 @@ echo "bwa mem \${Mo17_genome} \${fastq1} \${fastq2} -M -t 24 > output/Mo17/SAM/$
 #######################################################
 #####       Convert SAM to BAM and sort           #####
 #######################################################
-echo "samtools view -b -F 4 -S output/ABS/BAM/${i}_ABS.sam -o output/ABS/BAM/${i}_ABS.bam" >> ${out}  
-echo "samtools view -b -F 4 -S output/W22/BAM/${i}_W22.sam -o output/W22/BAM/${i}_W22.bam" >> ${out}  
-echo "samtools view -b -F 4 -S output/Mo17/BAM/${i}_Mo17.sam -o output/Mo17/BAM/${i}_Mo17.bam" >> ${out}  
+echo "samtools view -b -F 4 -S output/ABS/SAM/${i}_ABS.sam -o output/ABS/BAM/${i}_ABS.bam" >> ${out}  
+echo "samtools view -b -F 4 -S output/W22/SAM/${i}_W22.sam -o output/W22/BAM/${i}_W22.bam" >> ${out}  
+echo "samtools view -b -F 4 -S output/Mo17/SAM/${i}_Mo17.sam -o output/Mo17/BAM/${i}_Mo17.bam" >> ${out}  
 echo "samtools sort -o output/ABS/BAM/${i}_ABS_sorted.bam output/ABS/BAM/${i}_ABS.bam" >> ${out} 
 echo "samtools sort -o output/W22/BAM/${i}_ABS_sorted.bam output/W22/BAM/${i}_W22.bam" >> ${out}    
 echo "samtools sort -o output/Mo17/BAM/${i}_ABS_sorted.bam output/Mo17/BAM/${i}_Mo17.bam" >> ${out}    
-echo "rm output/ABS/BAM/${i}_ABS.bam output/W22/BAM/${i}_W22.bam output/Mo17/BAM/${i}_Mo17.bam" 
+echo "rm output/ABS/BAM/${i}_ABS.bam output/W22/BAM/${i}_W22.bam output/Mo17/BAM/${i}_Mo17.bam" >> ${out}  
 #######################################################
 #####       Filter reads lower than q20           #####
 #######################################################
@@ -91,9 +91,15 @@ echo "igvtools count -w 10000 output/Mo17/BAMQ20/${i}_Mo17_sorted_q20.bam output
 echo "bedtools genomecov -ibam output/ABS/BAM/${i}_ABS_sorted.bam -bg > output/ABS/BED/${i}_ABS_sorted.bed" >> ${out}
 echo "bedtools genomecov -ibam output/W22/BAM/${i}_W22_sorted.bam -bg > output/W22/BED/${i}_W22_sorted.bed" >> ${out}
 echo "bedtools genomecov -ibam output/Mo17/BAM/${i}_Mo17_sorted.bam -bg > output/Mo17/BED/${i}_W22_sorted.bed" >> ${out}
-echo "bedtools genomecov -ibam output/ABS/BAMQ20/${i}_ABS_sorted_q20.bam -bg > /output/ABS/BEDQ20/${i}_ABS_sorted.bed" >> ${out}
-echo "bedtools genomecov -ibam output/W22/BAMQ20/${i}_W22_sorted_q20.bam -bg > /output/W22/BEDQ20/${i}_W22_sorted.bed" >> ${out}
-echo "bedtools genomecov -ibam output/Mo17/BAMQ20/${i}_Mo17_sorted_q20.bam -bg > /output/Mo17/BEDQ20/${i}_Mo17_sorted.bed" >> ${out}
+echo "bedtools genomecov -ibam output/ABS/BAMQ20/${i}_ABS_sorted_q20.bam -bg > output/ABS/BEDQ20/${i}_ABS_sorted.bed" >> ${out}
+echo "bedtools genomecov -ibam output/W22/BAMQ20/${i}_W22_sorted_q20.bam -bg > output/W22/BEDQ20/${i}_W22_sorted.bed" >> ${out}
+echo "bedtools genomecov -ibam output/Mo17/BAMQ20/${i}_Mo17_sorted_q20.bam -bg > output/Mo17/BEDQ20/${i}_Mo17_sorted.bed" >> ${out}
+echo "awk '{print \$1,\$2,\$3,\$4,(\$3-\$2)*\$4}' OFS=\"\\t\" output/ABS/BED/${i}_ABS_sorted.bed > output/ABS/BED/${i}_ABS.bed" >> ${out}
+echo "awk '{print \$1,\$2,\$3,\$4,(\$3-\$2)*\$4}' OFS=\"\\t\" output/W22/BED/${i}_W22_sorted.bed > output/W22/BED/${i}_W22.bed" >> ${out}
+echo "awk '{print \$1,\$2,\$3,\$4,(\$3-\$2)*\$4}' OFS=\"\\t\" output/Mo17/BED/${i}_Mo17_sorted.bed > output/Mo17/BED/${i}_Mo17.bed" >> ${out}
+echo "awk '{print \$1,\$2,\$3,\$4,(\$3-\$2)*\$4}' OFS=\"\\t\" output/ABS/BEDQ20/${i}_ABS_sorted.bed > output/ABS/BEDQ20/${i}_ABS.bed" >> ${out}
+echo "awk '{print \$1,\$2,\$3,\$4,(\$3-\$2)*\$4}' OFS=\"\\t\" output/W22/BEDQ20/${i}_W22_sorted.bed > output/W22/BEDQ20/${i}_W22.bed" >> ${out}
+echo "awk '{print \$1,\$2,\$3,\$4,(\$3-\$2)*\$4}' OFS=\"\\t\" output/Mo17/BEDQ20/${i}_Mo17_sorted.bed > output/Mo17/BEDQ20/${i}_Mo17.bed" >> ${out}
 #######################################################
 #####             10kb-window files               #####
 #######################################################
@@ -103,10 +109,16 @@ echo "win10k_Mo17=/scratch/yz77862/MaizeGenome/Mo17_10k_win.bed" >> ${out}
 #######################################################
 #####             10kb-win genomecov              #####
 #########################################c##############
-echo "bedtools intersect -wa -wb -a \${win} -b \${genomecov}/${i}_genomecov.bed | bedtools groupby -c 8 -o sum > \${genomecov}\${i}_win_\${j}k_genomecov.bed1" >> ${out}
-echo "bedtools intersect -wa -wb -a \${win} -b \${genomecov}/${i}_q20_genomecov.bed | bedtools groupby -c 8 -o sum > \${genomecov}\${i}_win_\${j}k_q20_genomecov.bed1" >> ${out}
-echo "bedtools intersect -wa -wb -a \${win} -b \${genomecov}/${i}_win_\${j}k_genomecov.bed1 -v | awk '{print \$1,\$2,\$3,0}' OFS=\"\\t\"> \${genomecov}\${i}_win_\${j}k_genomecov.bed2" >> ${out} 
-echo "bedtools intersect -wa -wb -a \${win} -b \${genomecov}/${i}_win_\${j}k_q20_genomecov.bed1 -v | awk '{print \$1,\$2,\$3,0}' OFS=\"\\t\"> \${genomecov}\${i}_win_\${j}k_q20_genomecov.bed2" >> ${out} 
-echo "cat \${genomecov}/${i}_win_\${j}k_genomecov.bed1 \${genomecov}/${i}_win_\${j}k_genomecov.bed2 | sort -b -k1,1 -k2,2n -k3,3n  > \${genomecov}\${i}_win_\${j}k_q20_genomecov.bed" >> ${out} 
-echo "cat \${genomecov}/${i}_win_\${j}k_q20_genomecov.bed1 \${genomecov}/${i}_win_\${j}k_q20_genomecov.bed2 | sort -b -k1,1 -k2,2n -k3,3n  > \${genomecov}\${i}_win_\${j}k_q20_genomecov.bed" >> ${out} 
+echo "bedtools intersect -wa -wb -a \${win10k_ABS} -b output/ABS/BED/${i}_ABS.bed | bedtools groupby -c 8 -o sum > output/ABS/genomecov/${i}_ABS_10k.bed1" >> ${out}
+echo "bedtools intersect -wa -wb -a \${win10k_ABS} -b output/ABS/BED/${i}_ABS.bed -v | awk '{print \$1,\$2,\$3,0}' OFS=\"\\t\"> output/ABS/genomecov/${i}_ABS_10k.bed2" >> ${out} 
+echo "cat output/ABS/genomecov/${i}_ABS_10k.bed1 output/ABS/genomecov/${i}_ABS_10k.bed2 | sort -b -k1,1 -k2,2n -k3,3n  > output/ABS/genomecov/${i}_ABS_10k.bed" >> ${out}
+echo "rm output/ABS/genomecov/${i}_ABS_10k.bed2 output/ABS/genomecov/${i}_ABS_10k.bed1"  >> ${out}
+echo "bedtools intersect -wa -wb -a \${win10k_W22} -b output/W22/BED/${i}_W22.bed | bedtools groupby -c 8 -o sum > output/W22/genomecov/${i}_W22_10k.bed1" >> ${out}
+echo "bedtools intersect -wa -wb -a \${win10k_W22} -b output/W22/BED/${i}_W22.bed -v | awk '{print \$1,\$2,\$3,0}' OFS=\"\\t\"> output/W22/genomecov/${i}_W22_10k.bed2" >> ${out} 
+echo "cat output/W22/genomecov/${i}_W22_10k.bed1 output/W22/genomecov/${i}_W22_10k.bed2 | sort -b -k1,1 -k2,2n -k3,3n  > output/W22/genomecov/${i}_W22_10k.bed" >> ${out}
+echo "rm output/W22/genomecov/${i}_W22_10k.bed2 output/W22/genomecov/${i}_W22_10k.bed1"  >> ${out}
+echo "bedtools intersect -wa -wb -a \${win10k_Mo17} -b output/Mo17/BED/${i}_Mo17.bed | bedtools groupby -c 8 -o sum > output/Mo17/genomecov/${i}_Mo17_10k.bed1" >> ${out}
+echo "bedtools intersect -wa -wb -a \${win10k_Mo17} -b output/Mo17/BED/${i}_Mo17.bed -v | awk '{print \$1,\$2,\$3,0}' OFS=\"\\t\"> output/Mo17/genomecov/${i}_Mo17_10k.bed2" >> ${out} 
+echo "cat output/Mo17/genomecov/${i}_Mo17_10k.bed1 output/Mo17/genomecov/${i}_Mo17_10k.bed2 | sort -b -k1,1 -k2,2n -k3,3n  > output/Mo17/genomecov/${i}_Mo17_10k.bed" >> ${out}
+echo "rm output/Mo17/genomecov/${i}_Mo17_10k.bed2 output/Mo17/genomecov/${i}_Mo17_10k.bed1"  >> ${out}
 done < <(cut -f1 ${list} | grep -v 'skip' | sort -u)
