7106cc7f301fd37dc2cf6e86a115c0459a9f1b3c
Yibing <48507231+YibingZeng@users.noreply.github.com>
2024-08-26 10:07:27 -0400

    Update 04_makewindows.sh

--- Mapping/04_makewindows.sh
+++ Mapping/04_makewindows.sh
@@ -8,4 +8,7 @@ Mo17_fai=/scratch/yz77862/MaizeGenome/Zm-Mo17-REFERENCE-CAU-2.0_addname.fa.fai
 head ${Mo17_fai} > Mo17_genome_size
 bedtools makewindows -g Mo17_genome_size -w 100000 > Mo17_100k_win.bed
 
-W22_fai=/scratch/yz77862/MaizeGenome/
+##W22 downloaded from maizegdb can not generate the fai file and fai file can only be downloaded in the website 
+W22_fai=/scratch/yz77862/MaizeGenome/Zm-W22-REFERENCE-NRGENE-2.0.chr_scaffolds.fa.gz.fai
+head ${W22_fai} > W22_genome_size 
+bedtools makewindows -g W22_genome_size -w 100000 > W22_100k_win.bed
