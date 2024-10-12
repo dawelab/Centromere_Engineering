47fbc2cc4cdeb9e564597f7052700fd7d362295c
Yibing <48507231+YibingZeng@users.noreply.github.com>
2024-10-12 11:12:04 -0400

    Update 07_mRNA_mapping.sh

--- Mapping/07_mRNA_mapping.sh
+++ Mapping/07_mRNA_mapping.sh
@@ -11,9 +11,9 @@
 ml Trim_Galore/0.6.7-GCCcore-11.2.0 
 cd /scratch/yz77862/mRNA
 list=/scratch/yz77862/mRNA/list
-while read GENOME;do
-trim_galore --fastqc --gzip --paired ${GENOME}_R1_001.fastq.gz ${GENOME}_R2_001.fastq.gz -o . 
-done < <(cut -f1 ${list} | grep -v 'skip' | sort -u )
+#while read GENOME;do
+#trim_galore --fastqc --gzip --paired ${GENOME}_R1_001.fastq.gz ${GENOME}_R2_001.fastq.gz -o . 
+#done < <(cut -f1 ${list} | grep -v 'skip' | sort -u )
 
 
 ml BWA/0.7.17-GCCcore-11.3.0 
@@ -33,13 +33,13 @@ ABS_genome=/scratch/yz77862/ABS_PacBio_version1/AbsGenomePBHIFI_version_1.fa
 #####       Set up the value for input            #####
 #######################################################
 while read GENOME;do
-fastq1=/scratch/yz77862/mRNA/${GENOME}_R1_001_val_1.fq.gz
-fastq2=/scratch/yz77862/mRNA/${GENOME}_R2_001_val_2.fq.gz
+#fastq1=/scratch/yz77862/mRNA/${GENOME}_R1_001_val_1.fq.gz
+#fastq2=/scratch/yz77862/mRNA/${GENOME}_R2_001_val_2.fq.gz
 #######################################################
 #####       Map to different genomes             #####
 #######################################################
-bwa mem ${ABS_genome} ${fastq1} ${fastq2} -M -t 24  > ${output_dir}/${GENOME}_ABS.sam 
-samtools view -b -F 4 -S ${output_dir}/${i}_ABS.sam -o ${output_dir}/${GENOME}_ABS.bam
+#bwa mem ${ABS_genome} ${fastq1} ${fastq2} -M -t 24  > ${output_dir}/${GENOME}_ABS.sam 
+samtools view -b -F 4 -S ${output_dir}/${GENOME}_ABS.sam -o ${output_dir}/${GENOME}_ABS.bam
 samtools sort -o ${output_dir}/${GENOME}_ABS_sorted.bam ${output_dir}/${GENOME}_ABS.bam
 samtools view -q 20 -o ${output_dir}/${GENOME}_ABS_sorted_q20.bam ${output_dir}/${GENOME}_ABS_sorted.bam
 
