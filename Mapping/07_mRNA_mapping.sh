a03474028b69123ec1d118b4421dd21e932e6220
Yibing <48507231+YibingZeng@users.noreply.github.com>
2024-10-22 15:21:37 -0400

    Update 07_mRNA_mapping.sh

--- Mapping/07_mRNA_mapping.sh
+++ Mapping/07_mRNA_mapping.sh
@@ -11,15 +11,16 @@ OUT=/scratch/yz77862/mRNA/gene_guide/shell/${INPUT}.sh
     echo "#SBATCH --job-name=${INPUT}_mapping"   >> ${OUT}            
     echo "#SBATCH --partition=batch"   >> ${OUT} 
     echo "#SBATCH --nodes=1"   >> ${OUT}                  
-    echo "#SBATCH --ntasks=1"   >> ${OUT}              
+    echo "#SBATCH --ntasks=7"   >> ${OUT}              
     echo "#SBATCH --cpus-per-task=18"   >> ${OUT}          
-    echo "#SBATCH --mem=200G"   >> ${OUT}                  
+    echo "#SBATCH --mem=400G"   >> ${OUT}
+    echo "#SBATCH --ntasks-per-node=1" >> ${OUT}
     echo "#SBATCH --time=030:00:00"   >> ${OUT}             
     echo "#SBATCH --output=${INPUT}_fq_bam.out"   >> ${OUT}         
     echo "#SBATCH --error=${INPUT}_fq_bam.err"   >> ${OUT}         
     echo " "  >> ${OUT}  
-    echo "ml STAR" >> ${OUT}  
-    echo "cd /scratch/yz77862/mRNA/gene_guide/round1" >> ${OUT}
+    echo "ml  STAR/2.7.10b-GCC-11.3.0" >> ${OUT}  
+    echo "cd /scratch/yz77862/mRNA/gene_guide/round1" >> ${OUT}r
     echo " "  >> ${OUT}
     echo "thread=18"  >> ${OUT}  
     echo "index=/scratch/yz77862/ABS_Pacbio_index"  >> ${OUT}  
@@ -39,23 +40,3 @@ OUT=/scratch/yz77862/mRNA/gene_guide/shell/${INPUT}.sh
     echo "--outFilterMultimapNmax 10000" >> ${OUT}  
     echo "--sjdbGTFfile \${gtf}" >> ${OUT}  
     echo " "  >> ${OUT}
-    echo "cd /scratch/yz77862/mRNA/gene_guide/round2"  >> ${OUT}
-    echo " " >> ${OUT}
-    echo "SJ=/scratch/yz77862/mRNA/gene_guide/round1/${INPUT}_STARpass1/SJ.out.tab"  >> ${OUT}
-    echo "STAR \\"  >> ${OUT}
-    echo "--genomeDir \${index} \\"  >> ${OUT}
-    echo "--runThreadN \${thread} \\"  >> ${OUT}
-    echo "--sjdbFileChrStartEnd \${SJ} \\"  >> ${OUT}
-    echo "--runMode alignReads \\"  >> ${OUT}
-    echo "--readFilesIn \${read1} \${read2}\\"  >> ${OUT}
-    echo "--outSAMattributes All \\"  >> ${OUT}
-    echo "--outSAMmapqUnique 10 \\"  >> ${OUT}
-    echo "--outFilterMismatchNmax 3 \\"  >> ${OUT}
-    echo "--outFileNamePrefix ${INPUT}_round-2 \\"  >> ${OUT}
-    echo "--outBAMsortingThreadN 4 \\"  >> ${OUT}
-    echo "--outSAMtype BAM SortedByCoordinate \\"  >> ${OUT}
-    echo "--outFilterScoreMin 50 \\" >> ${OUT}  
-    echo "--outFilterMultimapNmax 10000 \\" >> ${OUT}  
-    echo "--outWigType bedGraph read1_5p"  >> ${OUT}
-   # sbatch ${OUT}
-done < <(cut -f1,2 ${list} | grep -v 'skip' | sort -u)
