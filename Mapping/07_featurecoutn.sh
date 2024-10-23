7ef168595120eda587f84296b2e48d37b720ada7
Yibing <48507231+YibingZeng@users.noreply.github.com>
2024-10-23 15:31:41 -0400

    Update 07_featurecoutn.sh

--- Mapping/07_featurecoutn.sh
+++ Mapping/07_featurecoutn.sh
@@ -1,2 +1,2 @@
 gtf=/scratch/yz77862/liftoff/A188_liftoff_ABS_cds.gtf
-/home/yz77862/apps/subread-1.6.0-Linux-x86_64/bin/featureCounts  -a ${gtf} -o L3_exon_count.counts *bam -M -f -t exon
+/home/yz77862/apps/subread-1.6.0-Linux-x86_64/bin/featureCounts  -a ${gtf} -o L3_exon_count.counts *bam -M -f -t gene
