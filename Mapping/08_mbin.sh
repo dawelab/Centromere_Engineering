216e58b90196ca1d2c0097b3a3a6d2445467e88a
Yibing <48507231+YibingZeng@users.noreply.github.com>
2024-10-24 13:47:41 -0400

    Update 08_mbin.sh

--- Mapping/08_mbin.sh
+++ Mapping/08_mbin.sh
@@ -13,8 +13,8 @@
 ml CGmapTools/0.1.2-foss-2022a
 cd /scratch/yz77862/EMmethyl
 
-for GENOME in KD4315-2 KD4315-4 KD4315-5 KD4315-8 yz306-2 yz306-3 yz306-5 yz306-6;do
-cgmaptools mbin -i ${GENOME}.CGmap.gz -CG -B 500000 -c 1  > ${GENOME}.mCG.mbin.500k.data
-cgmaptools mbin -i ${GENOME}.CGmap.gz -CHG -B 500000 -c 1  > ${GENOME}.mCHG.mbin.500k.data
-cgmaptools mbin -i ${GENOME}.CGmap.gz -CHH -B 500000 -c 1  > ${GENOME}.mCHH.mbin.500k.data
+for GENOME in KD4315-2 KD4315-4 KD4315-5 KD4315-8 yz306-2 yz306-3 yz306-5;do
+cgmaptools mbin -i ${GENOME}.CGmap.gz -C CG -B 10000 -c 1  > ${GENOME}.mCG.mbin.10k.data
+cgmaptools mbin -i ${GENOME}.CGmap.gz -C CHG -B 10000 -c 1  > ${GENOME}.mCHG.mbin.10k.data
+cgmaptools mbin -i ${GENOME}.CGmap.gz -C CHH -B 10000 -c 1  > ${GENOME}.mCHH.mbin.10k.data
 done
