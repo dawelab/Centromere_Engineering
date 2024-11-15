e70857730db3f7f1028a3426b954df7239ca5643
Yibing <48507231+YibingZeng@users.noreply.github.com>
2024-11-15 12:21:48 -0500

    Update 11_telo.sh

--- Mapping/11_telo.sh
+++ Mapping/11_telo.sh
@@ -8,29 +8,9 @@
 #SBATCH --output=trim_adaptor.out			  
 #SBATCH --error=trim_adaptor.err
 
-cd /scratch/yz77862/work_illu
+cd /scratch/yz77862/illumina_neo4Ls/data/fasta
+ml BLAST+
 
-ml BWA/0.7.17-GCCcore-11.3.0  
-ml BEDTools/2.29.2-GCC-8.3.0
-ml SAMtools/1.16.1-GCC-11.3.0
-ml IGV/2.16.1-Java-11
-
-win100k_ABS=/scratch/yz77862/ABS_PacBio_version1/AbsGenomePBHIFI_version_1_100k_win.bed
-win100k_W22=/scratch/yz77862/MaizeGenome/W22_100k_win.bed
-win100k_Mo17=/scratch/yz77862/MaizeGenome/Mo17_100k_win.bed 
-
-ABS_genome=/scratch/yz77862/ABS_PacBio_version1/AbsGenomePBHIFI_version_1.fa  
-#17-4-2-KAPA 4-3-KAPA 7-TRI-3-KAPA YZ306-1-KAPA YZ306-2-CENH3-1 YZ306-2-CENH3-2 YZ306-2-IgG-1 YZ306-2-IgG-2
-cd /scratch/yz77862/work
-for i in *_ABS_sorted.bam;do
-bedtools coverage -a  ${win100k_ABS} -b ${i}  -counts > ${i}_read_counts_100kb.txt
+for i in *.fq.gz.fa;do
+blastn -query ${i} -db ABS.fasta_blastdb -out ${i}_blast_diABS_result.xls -task blastn -outfmt 6
 done
-
-for i in *_Mo17_sorted.bam;do
-bedtools coverage -a  ${win100k_Mo17} -b ${i}  -counts > ${i}_read_counts_100kb.txt
-done
-
-for i in *_W22_sorted.bam;do
-bedtools coverage -a  ${win100k_W22} -b ${i}  -counts > ${i}_read_counts_100kb.txt
-done
-
