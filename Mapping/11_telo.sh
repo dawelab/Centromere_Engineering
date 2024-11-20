0a4258dd1d16cc44a50df6f288296426755bda6f
Yibing <48507231+YibingZeng@users.noreply.github.com>
2024-11-20 11:41:08 -0500

    Update 11_telo.sh

--- Mapping/11_telo.sh
+++ Mapping/11_telo.sh
@@ -8,9 +8,9 @@
 #SBATCH --output=trim_adaptor.out			  
 #SBATCH --error=trim_adaptor.err
 
-cd /scratch/yz77862/illumina_neo4Ls/data/fasta
-ml BLAST+
-
-for i in *.fq.gz.fa;do
-blastn -query ${i} -db ABS.fasta_blastdb -out ${i}_blast_diABS_result.xls -task blastn -outfmt 6
+cd /scratch/yz77862/illumina_neo4Ls/output/ABS/BAM
+ml BEDTools
+ABS_10k=/scratch/yz77862/ABS_PacBio_version1/AbsGenomePBHIFI_version_10k_win.bed
+for i in *bam;do
+bedtools intersect -a ${ABS_10k} -b ${i} -c -sorted > ${i}_10k.bed
 done
