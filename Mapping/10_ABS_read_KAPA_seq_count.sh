47f476c2fc277ffcf9de03bc22fd7913dbbc54ec
Yibing <48507231+YibingZeng@users.noreply.github.com>
2024-11-21 11:29:40 -0500

    Update 10_ABS_read_KAPA_seq_count.sh

--- Mapping/10_ABS_read_KAPA_seq_count.sh
+++ Mapping/10_ABS_read_KAPA_seq_count.sh
@@ -1,3 +1,5 @@
+wk '$3>=95' J721_R1_001_val_1_blast_diABS_result.xls | awk '{print $1,$7,$8}' OFS="\t" | uniq | sort -b -k1,1 -k2,2n -k3,3n | bedtools merge | wc -l
+
 for file in *.fa; do 
 echo "$(basename "$file") $(( $(wc -l < "$file") / 2 ))"
 done > read_countsummary.txt
