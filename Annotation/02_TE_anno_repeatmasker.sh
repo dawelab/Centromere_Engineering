e995bd233e897ef60a970503c3d986a0e9045b9e
Yibing <48507231+YibingZeng@users.noreply.github.com>
2024-12-01 16:14:00 -0500

    Update 02_TE_anno_repeatmasker.sh

--- Annotation/02_TE_anno_repeatmasker.sh
+++ Annotation/02_TE_anno_repeatmasker.sh
@@ -8,11 +8,11 @@
 #SBATCH --output=repeatmakser.out
 #SBATCH --error=repeatmakser.err
 
-cd /scratch/yz77862/ABS_PacBio_version1
+cd /scratch/yz77862/TE_annotation
 ml RepeatMasker/4.1.5-foss-2022a
 
 genome=/scratch/yz77862/ABS_PacBio_version1/AbsGenomePBHIFI_version_1.fa
-cds=/scratch/yz77862/B73v5_genome/Zm-B73-REFERENCE-NAM-5.0_Zm00001eb.1.cds.fa
+cds=/scratch/yz77862/TE_annotation/Zm-A188-REFERENCE-KSU-1.0_Zm00056aa.1.cds.fa
 
 #RepeatMasker -q -nolow -species Zea_mays -no_is ${genome}
 RepeatMasker -e ncbi -pa 18 -species Zea_mays -s -gff ${genome}
