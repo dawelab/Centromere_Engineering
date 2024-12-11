759f383e59b0238b9274163b2730522f9258a109
Yibing <48507231+YibingZeng@users.noreply.github.com>
2024-12-11 15:47:08 -0500

    Update 03_DEG.R

--- Plot/03_DEG.R
+++ Plot/03_DEG.R
@@ -1,4 +1,5 @@
 library('DESeq2')
+library(tidyverse)
 path="/Users/yibingzeng/Desktop/SyntheticCentromere/"
 L3_exon_count <- read.table(paste0(path,"L3_exon_count.counts"), header = T)
 count_data <- L3_exon_count[,c(1,7:14)]
@@ -23,14 +24,15 @@ count_data <- df_count[,-1]
 condition <- factor(c("control","control","control","control",
                       "eleven","eleven","eleven","eleven"))
 col_data <- data.frame(row.names = colnames(count_data), condition)
-
+45271
 dds <- DESeqDataSetFromMatrix(countData = count_data,colData = col_data,design = ~ condition)
-nrow(dds)K
+nrow(dds)
 dds_filter <- dds[ rowSums(counts(dds))>1, ]
 dds_out <- DESeq(dds_filter)
 res <- results(dds_out)
 summary(res)
-table(res$padj<0.05)
+table(res$padj<0.01)
+table(res$log2FoldChange>0)
 res_deseq <- res[order(res$padj),]
 #一般选取Foldchange值和经过FDR矫正过后的p值，取padj值(p值经过多重校验校正后的值)小于0.05，log2FoldChange大于1的基因作为差异基因集
 diff_gene_deseq2 <- subset(res_deseq, padj<0.05 & (log2FoldChange > 1 | log2FoldChange < -1))
@@ -77,13 +79,55 @@ CENH3_affected_gene <- data.frame(Row.names= c(
   "Zm00056aa025513"))
 deg_CENH3_affected_gene <- merge(CENH3_affected_gene,res_diff_data, by = "Row.names")
 deg_CENH3_affected_gene2 <- merge(CENH3_affected_gene,df_count, by = "Row.names")
-core_gene_set <- read.csv("https://raw.githubusercontent.com/dawelab/Natural-methylation-epialleles-correlate-with-gene-expression-in-maize/refs/heads/main/Data/B73.all.csv") %>% filter(class == "Core Gene") %>% select("gene")
-names(core_gene_set) <- "Row.names"
 row.names(deg_CENH3_affected_gene) <- deg_CENH3_affected_gene$Row.names
-deg_CENH3_affected_gene[deg_CENH3_affected_gene$pvalue<=0.05,]
+deg_CENH3_affected_gene[deg_CENH3_affected_gene$pvalue<=0.01,]
 pheatmap(deg_CENH3_affected_gene[,8:15])
 row.names(res_diff_data) <- res_diff_data$Row.names
 pheatmap(res_diff_data[sample(dim(res_diff_data)[1],22),8:15],cluster_rows=F)
 hist(res_diff_data$log2FoldChange)
 hist(deg_CENH3_affected_gene$log2FoldChange)
 
+#######Plotting for the overlaying histogram
+# Generate sample data
+set.seed(123)
+data1 <- res$log2FoldChange
+data2 <- deg_CENH3_affected_gene$log2FoldChange
+
+# Create data frame for ggplot
+df <- data.frame(
+  value = c(data1, data2),
+  group = c(rep("All", length(data1)), rep("CEN", length(data2)))
+)
+
+# Calculate densities for scaling
+hist1 <- hist(data1, breaks = 30, plot = FALSE)
+hist2 <- hist(data2, breaks = 30, plot = FALSE)
+scale_factor <- 23204/22
+
+# Plot
+ggplot(df, aes(x=value,fill=group)) +
+  geom_histogram(
+    data = subset(df, group == "All"),
+    aes( y = ..count..),
+    bins = 30,
+    fill = "blue",
+    alpha = 0.5
+  ) +
+  geom_histogram(
+    data = subset(df, group == "CEN"),
+    aes( y = ..count.. * scale_factor),
+    bins = 30,
+    fill = "orange",
+    alpha = 0.5
+  ) +
+  scale_y_continuous(
+    name = "All",
+    sec.axis = sec_axis(~ . / scale_factor, name = "CENH3")
+  ) +
+  xlim(-15,15) +
+  scale_fill_manual(
+    values = c("Histogram 1" = "blue", "Histogram 2" = "orange"),
+    name = "Legend"
+  ) +
+  theme_minimal() +
+  theme(legend.position = "top")
