a96b3acf9326eb3137db5d0b9cb7e47d9b10d45c
Yibing <48507231+YibingZeng@users.noreply.github.com>
2024-12-17 16:22:10 -0500

    Create LogisticRegression.R

---
+++ Stat/LogisticRegression.R
@@ -0,0 +1,15 @@
+library(readxl)
+library(ggplot2)
+
+DATA <- read_excel("")
+
+DATA$Recombination <- as.factor(DATA$Recombination)
+DATA$Identity <- as.factor(DATA$Identity)
+
+
+model = glm(Recombination ~ Identity, data=DATA, family="binomial")
+summary(model)
+
+ggplot(DATA, aes(x=Identity, fill = Recombination)) +
+  geom_bar(stat="count")
+
