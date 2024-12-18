5497d2701f983a45f0c848cb67563334cc03a032
Yibing <48507231+YibingZeng@users.noreply.github.com>
2024-12-17 22:01:56 -0500

    Update 08_mbin.sh

--- Mapping/08_mbin.sh
+++ Mapping/08_mbin.sh
@@ -14,13 +14,7 @@ ml CGmapTools/0.1.2-foss-2022a
 cd /scratch/yz77862/EMmethyl
 
 for GENOME in KD4315-2 KD4315-4 KD4315-5 KD4315-8 yz306-2 yz306-3 yz306-5;do
-cgmaptools mbin -i ${GENOME}.CGmap.gz -C CG -B 10000 -c 1  > ${GENOME}.mCG.mbin.10k.data
-cgmaptools mbin -i ${GENOME}.CGmap.gz -C CHG -B 10000 -c 1  > ${GENOME}.mCHG.mbin.10k.data
-cgmaptools mbin -i ${GENOME}.CGmap.gz -C CHH -B 10000 -c 1  > ${GENOME}.mCHH.mbin.10k.data
+cgmaptools mbin -i ${GENOME}.CGmap.gz -C CG -B 100000 -c 1  > ${GENOME}.mCG.mbin.10k.data
+cgmaptools mbin -i ${GENOME}.CGmap.gz -C CHG -B 100000 -c 1  > ${GENOME}.mCHG.mbin.10k.data
+cgmaptools mbin -i ${GENOME}.CGmap.gz -C CHH -B 100000 -c 1  > ${GENOME}.mCHH.mbin.10k.data
 done
-ml CGmapTools/0.1.2-foss-2022a
-GENOME=yz306-6
-cgmaptools mbin -i ${GENOME}.CGmap.gz -C CG -B 500000 -c 1  > ${GENOME}.mCG.mbin.500k.data
-cgmaptools mbin -i ${GENOME}.CGmap.gz -C CHG -B 500000 -c 1  > ${GENOME}.mCHG.mbin.500k.data
-cgmaptools mbin -i ${GENOME}.CGmap.gz -C CHH -B 500000 -c 1  > ${GENOME}.mCHH.mbin.500k.data
-
