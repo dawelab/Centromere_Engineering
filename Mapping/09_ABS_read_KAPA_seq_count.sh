1981b31d94347aa8eea5c23468cd09eea4200ee4
Yibing <48507231+YibingZeng@users.noreply.github.com>
2024-12-17 22:04:23 -0500

    Update 09_ABS_read_KAPA_seq_count.sh

--- Mapping/09_ABS_read_KAPA_seq_count.sh
+++ Mapping/09_ABS_read_KAPA_seq_count.sh
@@ -1,9 +1,10 @@
-wk '$3>=95' J721_R1_001_val_1_blast_diABS_result.xls | awk '{print $1,$7,$8}' OFS="\t" | uniq | sort -b -k1,1 -k2,2n -k3,3n | bedtools merge | wc -l
-
+ml BEDTools
+awk '$3>=95' J721_R1_001_val_1_blast_diABS_result.xls | awk '{print $1,$7,$8}' OFS="\t" | uniq | sort -b -k1,1 -k2,2n -k3,3n | bedtools merge | wc -l
+##Calculate the total read count from fasta
 for file in *.fa; do 
 echo "$(basename "$file") $(( $(wc -l < "$file") / 2 ))"
 done > read_countsummary.txt
-
+##Calculate the ABS containing reads from blast -outfmt 6 generated *xls files
 for file in *.xls; do
   if [ -f "$file" ]; then
     count=$(awk '$3 >= 95' "$file" | \
